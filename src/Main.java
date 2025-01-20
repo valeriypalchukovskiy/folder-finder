72ccdaeae120d26e608ac7f4084ae1892d6bef54
Valeriy Palchukovskiy <valeriy_palchukovskiy@mail.ru>
2025-01-20 14:16:48 +0300

    Dev1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.io.File;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        String folderPath = "/C:/Users/123/Desktop/Акты для работы ГБУ";
+
+        String folderPath2 = "/C:/Users/123/Desktop/nekoray";
+
+        File file = new File(folderPath2);
+
+        System.out.println(getFolderSize(file));
+    }
+
+    public static long  getFolderSize (File folder){
+        if (folder.isFile()) {
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file:files)
+        {
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }
