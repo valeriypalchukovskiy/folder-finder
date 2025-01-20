e28429547a859d1845c90f2ffe3c4d87eed39eed
Valeriy Palchukovskiy <valeriy_palchukovskiy@mail.ru>
2025-01-20 15:23:47 +0300

    Dev2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args)
     {
-        String folderPath = "/C:/Users/123/Desktop/Акты для работы ГБУ";
 
-        String folderPath2 = "/C:/Users/123/Desktop/nekoray";
+
+//
+//        MyThread thread = new MyThread(1);
+//        MyThread thread2 = new MyThread(2);
+//        thread.start();
+//        thread2.start();
+        String folderPath = "/C:/Users/123/Desktop/Акты для работы ГБУ";
+        String folderPath2 = "/C:/Users/123/Desktop";
 
         File file = new File(folderPath2);
+        long start = System.currentTimeMillis();
 
-        System.out.println(getFolderSize(file));
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size =  pool.invoke(calculator);
+        System.out.println(size);
+
+        long end = System.currentTimeMillis() - start;
+        System.out.println(end);
     }
 
     public static long  getFolderSize (File folder){
